05bede1d7faf39e6e3e67e46103ffb87becaf7ad
Alec Arza <bs192alec@gmail.com>
2025-03-06 18:39:20 -0500

    added file-based routing

---
+++ client/src/pages/index.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+const bestClass = 'Web Programming'
+</script>
+
+<template>
+  <main>
+    <h1 class="title">Welcome to {{ bestClass }}</h1>
+  </main>
+</template>
---
+++ client/src/pages/products/index.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="title">Products</h1>
+  </div>
+</template>
+
+<style scoped></style>
