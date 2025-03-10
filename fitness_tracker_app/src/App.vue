ac37fdecba67a93dc48d27a0019a680051aaa43d
Alec Arza <bs192alec@gmail.com>
2025-03-09 20:18:19 -0400

    Fitness tracker app midterm

--- fitness_tracker_app/src/App.vue
+++ fitness_tracker_app/src/App.vue
@@ -1,104 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+</script>
 
 <template>
-  <div id="app">
-    <nav class="navbar is-info" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://vitejs.dev/">
-          <img src="https://vuejs.org/images/logo.png" width="28" height="28" />
-        </a>
-      </div>
+  <header>
+    <NavBar />
+  </header>
 
-      <div id="navMenuColorprimary-example" class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a
-                  class="bd-tw-button button"
-                  data-social-network="Twitter"
-                  data-social-action="tweet"
-                  data-social-target="https://bulma.io"
-                  target="_blank"
-                  href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
-                >
-                  <span class="icon">
-                    <i class="fab fa-twitter"></i>
-                  </span>
-                  <span> Tweet </span>
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+  <div class="container body-container">
+    <RouterView />
   </div>
 </template>
-
-<style scoped>
-@import url('https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css');
-@import url('https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.css');
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+body {
+  background-color: aliceblue;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.body-container {
+  background-color: white;
+  height: 100vh;
+  padding: 1rem;
 }
 </style>
---
+++ fitness_tracker_app/src/components/NavBar.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isActive = ref(false)
+</script>
+
+<template>
+  <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://vitejs.dev/">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          :class="{ 'is-active': isActive }"
+          @click="isActive = !isActive"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" :class="{ 'is-active': isActive }">
+        <div class="navbar-start">
+          <RouterLink to="/my-activity" class="navbar-item">
+            <span class="icon">
+              <i class="fas fa-running"></i>
+            </span>
+            My Activity
+          </RouterLink>
+          <a class="navbar-item" href="#statistics">
+            <span class="icon">
+              <i class="fas fa-chart-line"></i>
+            </span>
+            Statistics
+          </a>
+          <a class="navbar-item" href="#friends-activity">
+            <span class="icon">
+              <i class="fas fa-users"></i>
+            </span>
+            Friend's Activity
+          </a>
+          <a class="navbar-item" href="#people-search">
+            <span class="icon">
+              <i class="fas fa-search"></i>
+            </span>
+            People Search
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="#admin">Admin</a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item" href="#admin">Users</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="field is-grouped">
+            <p class="control">
+              <a
+                class="bd-tw-button button"
+                data-social-network="Twitter"
+                data-social-action="tweet"
+                data-social-target="https://bulma.io"
+                target="_blank"
+                href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
+              >
+                <span class="icon">
+                  <i class="fab fa-twitter"></i>
+                </span>
+                <span> Tweet </span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
