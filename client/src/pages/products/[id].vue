ab2c370d9289483c7cba52b985a5fe25aa80de4f
Alec Arza <bs192alec@gmail.com>
2025-03-10 16:12:32 -0400

    listing products

--- client/src/pages/products/[id].vue
+++ client/src/pages/products/[id].vue
@@ -1,10 +1,39 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getOne } from '@/models/products'
+import { useRoute } from 'vue-router'
+
+const route = useRoute('/products/[id]')
+const product = getOne(Array.isArray(route.params.id) ? route.params.id[0] : route.params.id)
+</script>
 
 <template>
   <div>
-    <h1 class="title">Product {{ $route.params.id }}</h1>
+    <h1 class="title">
+      {{ product.title }}
+    </h1>
+    <div class="product">
+      <div class="product-info">
+        <p>{{ product.description }}</p>
+        <span class="price">${{ product.price }}</span>
+        <button class="button is-success">Add to cart</button>
+      </div>
+      <div class="product-images">
+        <img v-for="(i, index) in product.images" :key="index" :src="i" alt="product image" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.product {
+  display: flex;
+}
+
+.product-images {
+  display: flex;
+  flex-basis: 50%;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
