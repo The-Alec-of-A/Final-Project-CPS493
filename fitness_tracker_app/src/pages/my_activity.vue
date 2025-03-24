2c2e4e19448f7fd33982a789eab58cbd881d9cf3
Alec Arza <bs192alec@gmail.com>
2025-03-23 23:23:41 -0400

    Vue pages updated for improved functionality

--- fitness_tracker_app/src/pages/my_activity.vue
+++ fitness_tracker_app/src/pages/my_activity.vue
@@ -1,30 +1,58 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useStats } from '../models/useStats' // Assuming a composable for stats management
 
 const showAddActivityPanel = ref(false)
 const activityType = ref('')
 const activityMinutes = ref('')
 const activityDistance = ref('')
+const activities = ref<
+  { type: string; minutes: string; distance: string; markedForDeletion: boolean }[]
+>([]) // Store all activities
+const { addDistance } = useStats() // Function to update stats
 
 function submitActivity() {
-  console.log('Activity Submitted:', {
+  const activityData = {
     type: activityType.value,
     minutes: activityMinutes.value,
     distance: activityDistance.value,
-  })
+    markedForDeletion: false, // Default to not marked for deletion
+  }
+  console.log('Activity Submitted:', activityData)
+
+  activities.value.push(activityData) // Add to activities list
+
+  if (activityDistance.value) {
+    addDistance(parseFloat(activityDistance.value)) // Update stats with distance
+  }
+
   // Reset fields and close panel
   activityType.value = ''
   activityMinutes.value = ''
   activityDistance.value = ''
   showAddActivityPanel.value = false
 }
+
+function editActivity(index: number) {
+  const activity = activities.value[index]
+  activityType.value = activity.type
+  activityMinutes.value = activity.minutes
+  activityDistance.value = activity.distance
+  showAddActivityPanel.value = true
+
+  // Remove the activity being edited
+  activities.value.splice(index, 1)
+}
+
+function deleteActivity() {
+  activities.value = activities.value.filter((activity) => !activity.markedForDeletion) // Remove marked activities
+}
 </script>
 
 <template>
   <div class="button-container">
     <button class="add-button" @click="showAddActivityPanel = true">+ Add Activity</button>
-    <button class="delete-button">- Delete Activity</button>
-    <button class="edit-button">Edit Activity</button>
+    <button class="delete-button" @click="deleteActivity">- Delete Activity</button>
   </div>
 
   <div v-if="showAddActivityPanel" class="add-activity-panel">
@@ -38,12 +66,23 @@ function submitActivity() {
       <input type="number" v-model="activityMinutes" />
     </label>
     <label>
-      Distance (if applicable):
+      Distance (if applicable)(in miles):
       <input type="number" v-model="activityDistance" />
     </label>
     <div class="panel-buttons">
       <button @click="submitActivity">Submit</button>
       <button @click="showAddActivityPanel = false">Cancel</button>
+    </div>
+  </div>
+
+  <div v-for="(activity, index) in activities" :key="index" class="submitted-activity-panel">
+    <h3>Submitted Activity</h3>
+    <p><strong>Type:</strong> {{ activity.type }}</p>
+    <p><strong>Minutes:</strong> {{ activity.minutes }}</p>
+    <p><strong>Distance:</strong> {{ activity.distance }} miles</p>
+    <div class="submitted-activity-actions">
+      <button @click="editActivity(index)">Edit</button>
+      <label> <input type="checkbox" v-model="activity.markedForDeletion" /> Delete </label>
     </div>
   </div>
 </template>
@@ -109,5 +148,33 @@ function submitActivity() {
   border: none;
   padding: 5px 10px;
   cursor: pointer;
+}
+
+.submitted-activity-panel {
+  background-color: #e6f7ff;
+  border: 1px solid #91d5ff;
+  padding: 20px;
+  margin-top: 20px;
+  width: 300px;
+}
+
+.submitted-activity-actions {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+
+.submitted-activity-actions button {
+  background-color: rgb(6, 134, 181);
+  color: white;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.submitted-activity-actions label {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
 }
 </style>
