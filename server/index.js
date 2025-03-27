c491716bb2302cd78a61e21d545c10d9b4aed2ba
Alec Arza <bs192alec@gmail.com>
2025-03-27 14:28:22 -0400

    updated routing to account for v1

--- server/index.js
+++ server/index.js
@@ -1,21 +1,18 @@
-// Load the http module to create an http server.
-const express = require('express');
-const productsController = require('./controllers/products');
+const express = require('express')
+const productsController = require('./controllers/products')
+
 const PORT = 8000
 
 const app = express();
 
 app
-.get('/', (req, res) => {
-  res.send('Hello New Paltz, NY!!!\n');
-})
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz, NY!!!')
+  })
+  .use('/api/v1/products', productsController)
 
 // Listen on port 8000, IP defaults to
 //
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log(`Server running at http://localhost:${PORT}/`)
 });
-
-
-
-console.log('Hello World!');
