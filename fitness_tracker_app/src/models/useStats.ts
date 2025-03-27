7259be5867dfba1c64de17a621eec0c93d26f462
Alec Arza <bs192alec@gmail.com>
2025-03-27 14:00:55 -0400

    ts file edited

--- fitness_tracker_app/src/models/useStats.ts
+++ fitness_tracker_app/src/models/useStats.ts
@@ -3,6 +3,9 @@ import { ref } from 'vue'
 const todayDistance = ref(0)
 const weekDistance = ref(0)
 const allTimeDistance = ref(0)
+const todayDuration = ref(0)
+const weekDuration = ref(0)
+const allTimeDuration = ref(0)
 
 export function useStats() {
   function addDistance(distance: number) {
@@ -10,11 +13,20 @@ export function useStats() {
     weekDistance.value += distance
     allTimeDistance.value += distance
   }
+  function addTime(duration: number) {
+    todayDuration.value += duration
+    weekDuration.value += duration
+    allTimeDuration.value += duration
+  }
 
   return {
     todayDistance,
     weekDistance,
     allTimeDistance,
     addDistance,
+    todayDuration,
+    weekDuration,
+    allTimeDuration,
+    addTime,
   }
 }
