070651a9ede45643cadc541e66a452efb288464c
Alec Arza <bs192alec@gmail.com>
2025-04-24 13:48:40 -0400

    Getting individual items from server and DB

--- client/src/pages/products/[id].vue
+++ client/src/pages/products/[id].vue
@@ -1,39 +1,58 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { getOne } from '@/models/products'
+import { getOne, type Product } from '@/models/products'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute('/products/[id]')
-const product = getOne(Array.isArray(route.params.id) ? route.params.id[0] : route.params.id)
-</script>
+const product = ref<Product>();
 
+getOne(route.params.id)
+    .then((response) => {
+        product.value = response;
+    })
+</script>
 <template>
   <div>
-    <h1 class="title">
-      {{ product.title }}
-    </h1>
-    <div class="product">
-      <div class="product-info">
-        <p>{{ product.description }}</p>
-        <span class="price">${{ product.price }}</span>
-        <button class="button is-success">Add to cart</button>
-      </div>
+
+  <div class="product section" v-if="product">
       <div class="product-images">
-        <img v-for="(i, index) in product.images" :key="index" :src="i" alt="product image" />
+          <img v-for="i in product.images" :src="i" alt="product image" />
       </div>
-    </div>
+      <div class="product-info">
+          <h1 class="title">
+              {{ product.title }}
+          </h1>
+          <p>{{ product.description }}</p>
+          <span class="price">${{ product.price }}</span>
+          <button class="button is-success">Add to cart</button>
+      </div>
+
   </div>
+
+  <div v-else class="section">
+      <h1 class="title">Loading...</h1>
+  </div>
+
+</div>
 </template>
 
 <style scoped>
 .product {
-  display: flex;
+display: flex;
 }
 
 .product-images {
-  display: flex;
-  flex-basis: 50%;
-  flex-direction: column;
-  justify-content: space-between;
+display: flex;
+flex-basis: 50%;
+flex-direction: column;
+justify-content: space-between;
+}
+
+.price {
+font-size: 1.5rem;
+font-weight: bold;
+color: palevioletred;
+display: block;
+margin: 1em;
 }
 </style>
