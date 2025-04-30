65fa40d1019da9c56bf4ff0155631e11c452378e
Alec Arza <bs192alec@gmail.com>
2025-04-30 18:32:51 -0400

    user interface updated

--- fitness_tracker_app/src/models/user.ts
+++ fitness_tracker_app/src/models/user.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { api } from './myFetch'
 import { ref } from 'vue'
 import users from '@/data/users.json'
 
@@ -8,19 +9,36 @@ const existingUsernames = ref(['user1', 'user2', 'user3']) // Example existing u
 const usernameExists = ref(false)
 
 export interface User {
-  userId: number
-  userName: string
-  posts: any[]
-  summaries: any[]
-  friendsIds: any[]
+    id: number
+    firstName: string
+    lastName: string
+    email: string
+    age: number
+    gender: string
+    height: number
+    weight: number
+    role: string
+    image: string
 }
 
 export function getAll() {
-  return users
+  return api('user')
 }
 
-export function getOne(id: number) {
-  return users.users[id]
+export function get(id: number): Promise<User> {
+  return api(`user/${id}`)
+}
+
+export function update(user: User): Promise<User> {
+  return api(`users/${user.id}`).then(() => user)
+}
+
+export function remove(id: number): Promise<void> {
+  return api(`users/${id}`).then(() => undefined)
+}
+
+export function getImageUrl(user: User): string {
+  return new URL(`@/assets/${user.image}`, import.meta.url).toString();
 }
 
 export function handleSubmit() {
