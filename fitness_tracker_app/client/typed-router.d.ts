4edbced1a13739699e02ca5921b9b11a0f9d6200
Alec Arza <bs192alec@gmail.com>
2025-05-01 15:31:19 -0400

    Google Maps and Gemini

--- fitness_tracker_app/client/typed-router.d.ts
+++ fitness_tracker_app/client/typed-router.d.ts
@@ -19,8 +19,10 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/admin': RouteRecordInfo<'/admin', '/admin', Record<never, never>, Record<never, never>>,
     '/friends_activity': RouteRecordInfo<'/friends_activity', '/friends_activity', Record<never, never>, Record<never, never>>,
     '/my_activity': RouteRecordInfo<'/my_activity', '/my_activity', Record<never, never>, Record<never, never>>,
+    '/Profile/Id': RouteRecordInfo<'/Profile/Id', '/Profile/Id', Record<never, never>, Record<never, never>>,
     '/search': RouteRecordInfo<'/search', '/search', Record<never, never>, Record<never, never>>,
     '/sign-up': RouteRecordInfo<'/sign-up', '/sign-up', Record<never, never>, Record<never, never>>,
     '/stats': RouteRecordInfo<'/stats', '/stats', Record<never, never>, Record<never, never>>,
