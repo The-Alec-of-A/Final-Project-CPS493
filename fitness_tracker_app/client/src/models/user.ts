1f46d6356cf7009dacc7b27c2df56f9cbc5875e7
Alec Arza <bs192alec@gmail.com>
2025-05-03 13:06:07 -0400

    user attributes file

--- fitness_tracker_app/client/src/models/user.ts
+++ fitness_tracker_app/client/src/models/user.ts
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { api } from './myFetch'
 import { ref } from 'vue'
-import users from '@/data/users.json'
-
+import { api, post, remove, update } from './session'
 const emit = defineEmits(['add-username'])
 const username = ref('')
 const existingUsernames = ref(['user1', 'user2', 'user3']) // Example existing usernames
 const usernameExists = ref(false)
 
 export interface User {
-    id: number
+    id?: number
     firstName: string
     lastName: string
     email: string
@@ -19,21 +17,22 @@ export interface User {
     weight: number
     role: string
     image: string
+    friendsIds: number[]
 }
 
 export function getAll() {
   return api('user')
 }
 
-export function get(id: number): Promise<User> {
+export function getOne(id: number): Promise<User> {
   return api(`user/${id}`)
 }
 
-export function update(user: User): Promise<User> {
+export function updateUser(user: User): Promise<User> {
   return api(`users/${user.id}`).then(() => user)
 }
 
-export function remove(id: number): Promise<void> {
+export function deleteUser(id: number): Promise<void> {
   return api(`users/${id}`).then(() => undefined)
 }
 
@@ -41,11 +40,3 @@ export function getImageUrl(user: User): string {
   return new URL(`@/assets/${user.image}`, import.meta.url).toString();
 }
 
-export function handleSubmit() {
-  if (!usernameExists.value && username.value.trim()) {
-    // Emit event with new username
-    emit('add-username', username.value)
-    existingUsernames.value.push(username.value) // Add new username to the list
-    username.value = '' // Clear the input
-  }
-}
