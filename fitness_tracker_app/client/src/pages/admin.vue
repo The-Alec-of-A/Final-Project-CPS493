eb38a4c3c3be0c1745b8472ddb9083ab455f3346
Alec Arza <bs192alec@gmail.com>
2025-05-03 13:03:03 -0400

    user roles

---
+++ fitness_tracker_app/client/src/pages/admin.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AdminTable from '@/components/AdminStatus.vue'
+</script>
+
+<template>
+  <div>
+    <table>
+      <thead>
+        <AdminTable />
+      </thead>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
