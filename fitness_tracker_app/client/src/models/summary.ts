47832ab0fb860cfac2ee97381b0e6e1600352491
Alec Arza <bs192alec@gmail.com>
2025-05-03 13:05:37 -0400

    summary deletions added

--- fitness_tracker_app/client/src/models/summary.ts
+++ fitness_tracker_app/client/src/models/summary.ts
@@ -1,16 +1,36 @@
+import { api, post, remove } from "./session";
+
 export interface Summary {
-  name: string
-  note: string
-  type: string
+  id?: number;
+  name: string;
+  note: string;
+  type: string;
+  user_id: number;
 }
 
 export interface SummaryWeight extends Summary {
-  sets: number
-  reps: number
-  weight: number
+  sets: number;
+  reps: number;
+  weight: number;
 }
 
 export interface SummaryCardio extends Summary {
-  time: number
-  distance: number
+  time: number;
+  distance: number;
+}
+
+export async function getOneSummary(id:number): Promise<Summary> {
+  return api(`summaries/${id}`)
+}
+
+export async function getSummaryByUser(id:number): Promise<Summary[]> {
+  return api(`summaries/search/${id}?`)
+}
+
+export async function PostSummary(summary:Summary) {
+  return post('summaries', summary)
+}
+
+export async function deleteSummary(id: number){
+  return remove(`summaries/${id}`)
 }
