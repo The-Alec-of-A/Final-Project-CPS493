94f04ab10622ed94b58dd6fa641aacf3e133b9d5
Alec Arza <122430539+The-Alec-of-A@users.noreply.github.com>
2025-05-13 19:08:22 -0400

    Delete server directory meant for a different repo

--- server/models/supabase.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-module.exports = {
-    connect(){
-        return createClient(
-            process.env.SUPABASE_URL,
-            process.env.SUPABASE_SECRET_KEY
-        );
-    }
-}
