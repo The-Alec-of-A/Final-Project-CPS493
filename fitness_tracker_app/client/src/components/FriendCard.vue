33db1f3a2fd36c23a3a02e50107039a08353e7c6
Alec Arza <bs192alec@gmail.com>
2025-05-15 01:43:11 -0400

    link to friend data

---
+++ fitness_tracker_app/client/src/components/FriendCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { User } from '@/models/user';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+
+const props = defineProps<{ friend: User }>()
+
+</script>
+
+
+<template>
+<div class="m-5 friend p-0 m-0">
+    <RouterLink :to="`Profile/${props.friend.id}`">
+      <p class="is-size-5"><FontAwesomeIcon :icon="faCircleUser"/> {{ props.friend.firstName + " " + props.friend.lastName }}</p>
+    </RouterLink>
+  </div>
+  </template>
