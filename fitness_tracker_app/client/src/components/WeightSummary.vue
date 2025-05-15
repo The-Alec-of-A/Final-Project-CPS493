5b00000893c702d7f8c19f4179d43ea80dc6b7ce
Alec Arza <bs192alec@gmail.com>
2025-05-15 01:59:28 -0400

    code added to show weight stats

---
+++ fitness_tracker_app/client/src/components/WeightSummary.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { currentUser } from '@/models/session';
+import { deleteSummary, type Summary, type SummaryWeight } from '@/models/summary'
+
+const props = defineProps<{data: Summary}>()
+
+function remove() {
+  deleteSummary(Number(props.data.id))
+}
+</script>
+
+<template>
+  <section class="section py-2">
+    <div class="card has-background-grey-dark">
+      <div class="card-header has-background-grey-darker">
+        <p class="card-header-title">Exercise Summary</p>
+        <button v-if="currentUser.id as number == props.data.user_id" class="delete is-right-aligned" @click="remove()"></button>
+      </div>
+      <div
+        class="container has-text-centered is-title is-capitalized is-size-4 has-text-white has-text-weight-bold"
+      >
+        {{ (data as SummaryWeight).name }}
+      </div>
+      <div class="columns card-content">
+        <div class="column">
+          <div class="container">
+            <h3 class="title is-4 has-text-centered">
+              {{ (data as SummaryWeight).reps }} Reps X <br />
+              {{ (data as SummaryWeight).sets }} Sets
+            </h3>
+            <p class="subtitle is-5 has-text-centered">Work Done</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="container">
+            <h3 class="title is-3 has-text-centered">{{ (data as SummaryWeight).weight }} lbs</h3>
+            <p class="subtitle is-5 has-text-centered">Weight</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="container">
+            <h3 class="title is-3 has-text-centered">
+              {{
+                (data as SummaryWeight).reps *
+                (data as SummaryWeight).sets *
+                (data as SummaryWeight).weight
+              }}
+              lbs
+            </h3>
+            <p class="subtitle is-5 has-text-centered">Total Weight</p>
+          </div>
+        </div>
+      </div>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
