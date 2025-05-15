638036f4ed87e1017f6643270f18e321ff4f5537
Alec Arza <bs192alec@gmail.com>
2025-05-15 01:39:02 -0400

    user's friend list added

--- fitness_tracker_app/client/src/models/user.ts
+++ fitness_tracker_app/client/src/models/user.ts
@@ -15,28 +15,49 @@ export interface User {
     gender: string
     height: number
     weight: number
-    role: string
     image: string
     friendsIds: number[]
+    role?: string
 }
 
-export function getAll() {
+export async function getAll(): Promise<User[]> {
   return api('user')
 }
 
-export function getOne(id: number): Promise<User> {
+export async function getOne(id: number): Promise<User> {
   return api(`user/${id}`)
 }
 
-export function updateUser(user: User): Promise<User> {
+export async function updateUser(user: User): Promise<User> {
   return api(`users/${user.id}`).then(() => user)
 }
 
-export function deleteUser(id: number): Promise<void> {
+export async function deleteUser(id: number): Promise<void> {
   return api(`users/${id}`).then(() => undefined)
+}
+
+export async function postUser(newUser: User) {
+    return post('users/',newUser)
+}
+
+export async function getFriendsOfUser(id: number): Promise<User[]> {
+    const user = await getOne(id)
+    const friends = await Promise.all(
+        user.friendsIds.map(friendId => getOne(friendId))
+    )
+    return friends
+}
+
+export async function getUsersWithFriend(id: number): Promise<User[]> {
+    return api(`users/search/${id}?`)
+}
+
+export async function updateFriends(id:number, friendId: number) {
+    const currentList = (await getOne(id)).friendsIds
+    currentList.push(friendId)
+    return update(`users/${id}`, {"friends_Ids": currentList})
 }
 
 export function getImageUrl(user: User): string {
   return new URL(`@/assets/${user.image}`, import.meta.url).toString();
 }
-
