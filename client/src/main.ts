8b142f6e64631ebe26d91d9180c37e5ccc8829c7
Alec Arza <bs192alec@gmail.com>
2025-05-30 00:54:02 -0400

    removeed Buefy error

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,17 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Buefy from 'buefy'
 import Oruga from '@oruga-ui/oruga-next';
 
 const app = createApp(App)
 
 app
   .use(router)
-  .use(Buefy, {
-    defaultIconPack: 'fas',
-  })
-
   .use(Oruga)
 
 app.mount('#app')
